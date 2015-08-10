d45beda7067eeb41ef8993987be10898cf6cf326
John Stevenson <john@jr0cket.co.uk>
2015-08-10 20:24:09 +0100

    a running jetty webapp

--- src/extreme_hello_world/core.clj
+++ src/extreme_hello_world/core.clj
@@ -2,7 +2,14 @@
   (:require [ring.adapter.jetty :as jetty])
   (:gen-class))
 
+(defn hello-world [request]
+  (println request)
+  {:status 200
+   :body "Hello World extreme !!"
+   :headers []})
+
 (defn -main
   "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+  [port]
+  (jetty/run-jetty hello-world
+                   {:port (Integer. port)}))
