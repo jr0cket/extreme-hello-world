30354aeb0261aeb342f9f5cf2f91323dfd179455
John Stevenson <john@jr0cket.co.uk>
2015-08-10 21:12:43 +0100

    we have a button

--- src/extreme_hello_world/core.clj
+++ src/extreme_hello_world/core.clj
@@ -1,15 +1,44 @@
 (ns extreme-hello-world.core
-  (:require [ring.adapter.jetty :as jetty])
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer :all]
+            [ring.middleware.params :refer [wrap-params]])
   (:gen-class))
 
 (defn hello-world [request]
-  (println request)
-  {:status 200
-   :body "Hello World extreme !!"
-   :headers []})
+  (prn request)
+  (if (= :get (request :request-method))
+    {:status 200 :body "<form method='post' action='/'><input name='name'></input><input type='submit'></input>"}
+    {:status 200
+     :body (-> request
+               :params ,,,
+               (get ,,, "name")
+               (str ,,, " welcome to our hello work experience"))
+     :headers []}))
+
+
+;; (str "Hello.. " (get (get request :params) "name"))
+
+;; (def littlemap {:name john
+;;                 :age 21
+;;                 :address {:street "erickson" :postcode "BR2"}})
+
+;; (littlemap :name) ;;=>john
+;; (:name littlemap)
+;; (:postcode (:address littlemap))
+
+;; (get map key)
+;; (get-in map [key key])
+
+
 
 (defn -main
   "I don't do a whole lot."
   [port]
-  (jetty/run-jetty hello-world
+  (jetty/run-jetty (wrap-params (wrap-reload hello-world))
                    {:port (Integer. port)}))
+
+
+;; {key value}
+;; {"key" "value"} ;; from the URL
+;; {:keyword "value"} ;; common approach in Clojure
+;; {:keyword {:key "value"}}
